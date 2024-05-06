573fd8e4c37785a057a43b699bdd48d5ac172103
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-05-06 09:24:35 +0500

    Commit

--- apps/forms.py
+++ apps/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.forms import ModelForm
 from apps.models import Contact
 
 
-class InputContact(forms.Form):
-    first_name = forms.CharField(label="First Name", max_length=255)
-    last_name = forms.CharField(label="Last Name", max_length=255)
-    country = forms.CharField(label="Your Country", max_length=255)
-    subject = forms.CharField(label="Subject", max_length=500)
+# class InputContact(forms.Form):
+#     first_name = forms.CharField(label="First Name", max_length=255)
+#     last_name = forms.CharField(label="Last Name", max_length=255)
+#     country = forms.CharField(label="Your Country", max_length=255)
+#     subject = forms.CharField(label="Subject", max_length=500)
+
+class InputContact(ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
--- apps/views.py
+++ apps/views.py
@@ -7,13 +7,14 @@ def forms(request):
     if (request.method == 'POST'):
         form = InputContact(request.POST)
         if (form.is_valid()):
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            country = form.cleaned_data['country']
-            subject = form.cleaned_data['subject']
-            student = Contact.objects.create(first_name=first_name, last_name=last_name, country=country,
-                                             subject=subject)
-            student.save()
+            # first_name = form.cleaned_data['first_name']
+            # last_name = form.cleaned_data['last_name']
+            # country = form.cleaned_data['country']
+            # subject = form.cleaned_data['subject']
+            # student = Contact.objects.create(first_name=first_name, last_name=last_name, country=country,
+            #                                  subject=subject)
+            # student.save()
+            form.save()
             return render(request, "created_success.html")
 
     contact_form = InputContact()
